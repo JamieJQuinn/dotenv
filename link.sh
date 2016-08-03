e8ade887fcc8945a70df6b2ccda6be84dcb68553
Jamie Quinn <jjquinn42@gmail.com>
2016-08-03 13:47:58 +0100

    Added fish linkage

--- link.sh
+++ link.sh
@@ -1,8 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env fish
+
+# Get working directory
+set DIR (cd (dirname (status -f)); and pwd) 
 
 # Vim/Nvim
 mkdir -p ~/.vim
 mkdir -p ~/.config
 ln -s ~/.vim ~/.config/nvim
-ln -sr .vimrc ~/.vimrc
+ln -s $DIR/.vimrc ~/.vimrc
 ln -s ~/.vimrc ~/.config/nvim/init.vim
+
+# Fish
+mkdir -p ~/.config/fish
+ln -s $DIR/config.fish.global ~/.config/fish/config.fish.global
+if test -e ~/.config/fish/config.fish
+  echo "Local fish config exists already"
+else
+  echo ". ~/.config/fish/config.fish.global" >> ~/.config/fish/config.fish
+end

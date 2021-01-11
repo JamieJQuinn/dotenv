2d3c44f009ac51be6af500754f6b5b1d40a6717b
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-01-11 13:35:28 +0000

    Replaced fish with better bash

--- install.sh
+++ install.sh
@@ -12,24 +12,10 @@ mkdir -p ~/.vim/spell
 ln -s $DIR/spell_file ~/.vim/spell/en.utf-8.add
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 
-# Fish
-mkdir -p ~/.config/fish
-ln -s $DIR/config.fish.global ~/.config/fish/config.fish.global
-if [ -e "~/.config/fish/config.fish" ]
-then
-  echo "Local fish config exists already"
-else
-  echo ". ~/.config/fish/config.fish.global" >> ~/.config/fish/config.fish
-fi
-
-#mkdir -p ~/.config/zathura
-#ln -s $DIR/zathurarc ~/.config/zathura/zathurarc
-
-mkdir -p ~/.config/kitty
-ln -s $DIR/kitty.conf ~/.config/kitty/kitty.conf
-
-#mkdir -p ~/.config/ranger
-#ln -s $DIR/rc.conf ~/.config/ranger/rc.conf
+# Bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+rm ~/.bashrc
+ln -s $DIR/.bashrc ~/.bashrc
 
 mkdir -p ~/scripts
 cp scripts/* ~/scripts

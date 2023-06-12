c640237e2b670e04f154e051ff2932b589c21209
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2023-06-12 19:55:03 +0100

    Add pacman installs for basic new install

---
+++ pacman_installs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo pacman -Syu
+sudo pacman -S git fish borgmatic thunderbird keepassxc neovim
+sudo pacman -S lolcat figlet
+yay -S autofs slack-desktop teams-for-linux prismlauncher nordvpn-bin

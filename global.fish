9fb60e088d2992adb2bafb2cb49cb3aef73441e9
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2023-08-30 11:09:51 +0100

    Replace aliases with scripts and add config aliases

--- global.fish
+++ global.fish
@@ -12,17 +12,19 @@ alias vim nvim
 ## Todos
 set -x TODO_DIR ~/todo
 alias extract_todos "grep -P '\- \[.?\]'"
-alias todo "cd $TODO_DIR; vim todo.txt"
 alias todo-today "grep '(A)' $TODO_DIR/todo.txt"
 
 # Notes
 set -x NOTES_DIR ~/notes
-alias notes "cd $NOTES_DIR; vim -c 'Files'"
+
+# Configs
+alias vimcfg "cd ~/.config/nvim; nvim +'Telescope find_files' ."
+alias fishcfg "cd ~/.config/fish; nvim +'Telescope find_files' ."
+alias kittycfg "cd ~/.config/kitty; nvim kitty.conf"
 
 # Useful aliases
 alias timesheet "vim ~/timesheet.md"
-alias wiki "vim -c VimwikiIndex"
-alias wiki_new "vim -c ZettelNewBibtex"
+
 alias a 'xdg-open'
 alias connect_to_home_vnc "ssh -C -L 5901:home:5901 -N home-pi -v &; sleep 2; vncviewer localhost:1"
 

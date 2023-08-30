e2964fd1591e6c77fff6449a61e005a88d6eb8cd
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2023-08-30 11:21:51 +0100

    Add kitty config and update installer

--- install.sh
+++ install.sh
@@ -3,12 +3,33 @@
 # Get working directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+CONFIG_DIR=~/.config
+mkdir -p "$CONFIG_DIR"
+
 # Vim/Nvim
-mkdir -p ~/.config
-ln -s $DIR/nvim ~/.config/nvim
+NVIM_CONFIG_DIR=$CONFIG_DIR/nvim
+if [ ! -d "$NVIM_CONFIG_DIR" ]; then
+  ln -s "$DIR/nvim" "$NVIM_CONFIG_DIR"
+else
+  echo "$NVIM_CONFIG_DIR already exists. Skipping."
+fi
 
 # Fish
-mkdir -p ~/.config/fish
-ln -s $DIR/global.fish ~/.config/fish/conf.d/global.fish
+mkdir -p ~/.config/fish/conf.d
+ln -s "$DIR/global.fish" ~/.config/fish/conf.d/global.fish
 
-ln -s $DIR/scripts ~/scripts
+# Scripts
+SCRIPTS_DIR=~/scripts
+if [ ! -d "$SCRIPTS_DIR" ]; then
+  ln -s "$DIR/scripts" "$SCRIPTS_DIR"
+else
+  echo "$SCRIPTS_DIR already exists. Skipping."
+fi
+
+# Kitty
+KITTY_CONFIG_DIR=$CONFIG_DIR/kitty
+if [ ! -d "$KITTY_CONFIG_DIR" ]; then
+  ln -s "$DIR/kitty" "$KITTY_CONFIG_DIR"
+else
+  echo "$KITTY_CONFIG_DIR already exists. Skipping."
+fi

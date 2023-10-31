9708072f8a04230f6c13e7cabb357b25c5146687
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2023-10-31 17:54:13 +0000

    update keybindings

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -16,9 +16,6 @@ nm('<c-j>', '<c-w><c-j>' )
 nm('<c-k>', '<c-w><c-k>' )
 nm('<leader>a', '<cmd>AerialToggle!<CR>')
 nm('vv', 'viw')
-nm('<leader>pl', 'i[](<esc>pa)<esc>T[i')
-nm('<leader>p`', 'i`<esc>pa`<esc>')
-nm('<leader>pi', 'i![](<esc>pa)<esc>')
 
 -- Commenting
 nm('<c-/>', '<Plug>(comment_toggle_linewise_current)')
@@ -42,17 +39,17 @@ nm('gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')                              
 nm('gd', '<cmd>Telescope lsp_definitions<CR>')                            -- Goto declaration
 nm('<leader>O', '<cmd>Telescope oldfiles<CR>')                                   -- Show recent files
 nm('<leader>o', '<cmd>Telescope git_files<CR>')                                  -- Search for a file in project
-nm('<leader>pp', '<cmd>Telescope find_files<CR>')                                 -- Search for a file (ignoring git-ignore)
+nm('<leader>p', '<cmd>Telescope find_files<CR>')                                 -- Search for a file (ignoring git-ignore)
 -- nm('<leader>i', '<cmd>Telescope jumplist<CR>')                                   -- Show jumplist (previous locations)
 -- nm('<leader>b', '<cmd>Telescope git_branches<CR>')                               -- Show git branches
 nm('<leader>f', '<cmd>Telescope live_grep<CR>')                                  -- Find a string in project
-nm('<leader>l', '<cmd>Telescope buffers<CR>')                                    -- Show all buffers
+nm('<leader>b', '<cmd>Telescope buffers<CR>')                                    -- Show all buffers
 -- nm('<leader>a', '<cmd>Telescope<CR>')                                            -- Show all commands
 nm('<leader>t', '<cmd>Telescope lsp_dynamic_workspace_symbols<CR>')              -- Search for dynamic symbols
 -- }}}
 
 -- Trouble {{{
-nm('<leader>x', '<cmd>TroubleToggle<CR>')                                         -- Show all problems in project (with help of LSP)
+nm('<c-x>', '<cmd>TroubleToggle<CR>')                                         -- Show all problems in project (with help of LSP)
 nm('gr', '<cmd>Trouble lsp_references<CR>')                                       -- Show use of object in project
 -- }}}
 

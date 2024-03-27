757274ec77e00a5ab8504afa1df8e5cf3a86dbb8
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2024-03-27 15:06:41 +0000

    Add example keybindings for git

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -98,6 +98,46 @@ return {
           delay = 1000,
           ignore_whitespace = false,
         },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          -- Actions
+          map('n', '<leader>hs', gs.stage_hunk)
+          map('n', '<leader>hr', gs.reset_hunk)
+          map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+          map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
+
+          -- Text object
+          map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
       })
     end
   },
@@ -160,7 +200,7 @@ return {
 
       disable_frontmatter = true,
       ui = {
-        enable = false,
+        enable = true,
       },
 
       completion = {
@@ -188,7 +228,7 @@ return {
       },
 
       daily_notes = {
-        folder = "notes/journal",
+        folder = "journal",
         date_format = "%Y-%m-%d",
       },
 
@@ -308,7 +348,21 @@ return {
     }
   },
 
-  "tpope/vim-fugitive",
+  -- "tpope/vim-fugitive",
+  {
+      "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+      -- optional for floating window border decoration
+      dependencies = {
+          "nvim-lua/plenary.nvim",
+      },
+  },
   {
     "AndrewRadev/switch.vim",
     ft = "markdown",

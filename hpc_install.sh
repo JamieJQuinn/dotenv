775a0e5d39b33457897d17d019bd6e7524739402
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2024-03-27 16:42:04 +0000

    Add script for installing my usual tools on HPC

---
+++ hpc_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+NEOVIM_VERSION=0.9.5
+
+mkdir -p ~/.bin
+wget "https://github.com/neovim/neovim/releases/download/v$NEOVIM_VERSION/nvim.appimage" -O ~/.bin/nvim.appimage.$NEOVIM_VERSION
+chmod u+x ~/.bin/nvim.appimage.$NEOVIM_VERSION
+ln -s ~/.bin/nvim.appimage.$NEOVIM_VERSION ~/.bin/nvim

e2d04fccebe1810a9830b8332669d6059c005c48
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2024-03-28 13:53:14 +0000

    Refactor install script

--- install.sh
+++ install.sh
@@ -6,41 +6,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIG_DIR=~/.config
 mkdir -p "$CONFIG_DIR"
 
-# Vim/Nvim
-NVIM_CONFIG_DIR=$CONFIG_DIR/nvim
-if [ ! -d "$NVIM_CONFIG_DIR" ]; then
-  ln -s "$DIR/.config/nvim" "$NVIM_CONFIG_DIR"
-else
-  echo "$NVIM_CONFIG_DIR already exists. Skipping."
-fi
+for item in .config/nvim .config/kitty .config/i3 scripts .tmux.conf; do
+  if [ ! -d "$item" ]; then
+    ln -s "$DIR/$item" ~/"$item"
+  else
+    echo "~/$item already exists. Skipping."
+  fi
+done
 
 # Fish
 mkdir -p ~/.config/fish/conf.d
 ln -s "$DIR/global.fish" ~/.config/fish/conf.d/global.fish
-
-# Scripts
-SCRIPTS_DIR=~/scripts
-if [ ! -d "$SCRIPTS_DIR" ]; then
-  ln -s "$DIR/scripts" "$SCRIPTS_DIR"
-else
-  echo "$SCRIPTS_DIR already exists. Skipping."
-fi
-
-# Kitty
-KITTY_CONFIG_DIR=$CONFIG_DIR/kitty
-if [ ! -d "$KITTY_CONFIG_DIR" ]; then
-  ln -s "$DIR/.config/kitty" "$KITTY_CONFIG_DIR"
-else
-  echo "$KITTY_CONFIG_DIR already exists. Skipping."
-fi
-
-# Tmux
-ln -s "$DIR/.tmux.conf" ~/.tmux.conf
-
-# i3
-I3_CONFIG_DIR=$CONFIG_DIR/i3
-if [ ! -d "$I3_CONFIG_DIR" ]; then
-  ln -s "$DIR/.config/i3" "$I3_CONFIG_DIR"
-else
-  echo "$I3_CONFIG_DIR already exists. Skipping."
-fi

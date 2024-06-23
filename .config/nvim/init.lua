ad07fdfa44ee9c3ef8739688d68cb5e3f1617d87
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2024-06-23 15:50:12 +0100

    Disable automatic loading of changed vim settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,7 +12,13 @@ require "keybindings"
 
 -- Plugin management {{{
 local lazy = require("lazy")
-lazy.setup("plugins")
+local opts = {
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = false,
+  },
+}
+lazy.setup("plugins", opts)
 -- }}}
 
 -- vim:tabstop=2 shiftwidth=2 expandtab syntax=lua foldmethod=marker foldlevelstart=0 foldlevel=0

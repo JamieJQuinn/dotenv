358e44771e558260a3f1bc95471f4a1eda65001a
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2025-03-31 16:03:51 +0100

    Update scripts

--- scripts/check_battery.sh
+++ scripts/check_battery.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+if acpi | grep "Charging"; then exit 0; fi
+
 battery_level=$(acpi | grep -oe '[0-9]*%' | grep -oe '[0-9]*')
-trigger_pc=90
+trigger_pc=15
 
 if (( "$battery_level" <= "$trigger_pc" )); then
   notify-send -w "Battery level: $battery_level" -u CRITICAL

6f2920187ad61453078dfde6fea28c83a3f4459f
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2025-08-18 10:38:24 +0100

    Update config & pacman installs

--- pacman_installs.sh
+++ pacman_installs.sh
@@ -3,4 +3,4 @@
 sudo pacman -Syu
 sudo pacman -S git fish borgmatic thunderbird keepassxc neovim syncthing ttf-hack-nerd wmctrl ttf-profont-nerd yazi xsel exiftool
 sudo pacman -S lolcat figlet
-yay -S slack-desktop nordvpn-bin python-pywal16
+yay -S slack-desktop nordvpn-bin python-pywal16 dragon-drop

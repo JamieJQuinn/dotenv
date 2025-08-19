7f06abcd34ff6e3bd47fd4a338ec6a5ee40dc431
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2025-08-19 16:43:56 +0100

    Add xbacklight to pacman install

--- pacman_installs.sh
+++ pacman_installs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo pacman -Syu
-sudo pacman -S git fish borgmatic thunderbird keepassxc neovim syncthing ttf-hack-nerd wmctrl ttf-profont-nerd yazi xsel exiftool
+sudo pacman -S git fish borgmatic thunderbird keepassxc neovim syncthing ttf-hack-nerd wmctrl ttf-profont-nerd yazi xsel exiftool xorg-xbacklight
 sudo pacman -S lolcat figlet
 yay -S slack-desktop nordvpn-bin python-pywal16 dragon-drop
